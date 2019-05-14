6363d1ad7acd154f8ee5a7cb81fc3dc6864eb155
lailiqiang <liqianglai@gmail.com>
2019-05-14 09:49:55 +0800

    添加页面过渡样式，添加全局样式文件

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
 import Router from "./routes";
+import "./assets/stylesheets/_reset.css";
+import "./assets/stylesheets/common.less";
 
 ReactDOM.render(<Router />, document.getElementById("root"));
 
